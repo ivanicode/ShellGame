dea216f9c792ac8a3e93af19ed627ec0414e650b
Ivani <katarzyna.stosio@gmail.com>
2021-02-22 17:06:12 +0100

    kittys switches and ball shows randomly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,15 @@ import {useState} from 'react'
 
 function App() {
 
-  const [newKittyPosition, setNewKittyPosition] = useState(0)
+  const positionConfigurations = [
+    [100, 210, 320],
+    [100, 320, 210],
+    [210, 320, 100],
+    [210, 100, 320],
+    [320, 210, 100],
+    [320, 100, 210]
+  ]
+  const [newKittyPosition, setNewKittyPosition] = useState(positionConfigurations[0])
   const [kittyWithBall, setKittyWithBall] = useState(-1)
   const [kittyWithBallIsVisible, setKittyWithBallIsVisible] = useState(false)
   const [ballLeft, setBallLeft] = useState(0)
@@ -17,7 +25,12 @@ function App() {
     })*/
   }
   function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    let randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    }  while (randomNumber === getRandomIntInclusive.last)
+    getRandomIntInclusive.last = randomNumber
+    return randomNumber;
   }
   function buttonClicked(){
     const whichKitty = getRandomIntInclusive(0, 2)
@@ -27,7 +40,15 @@ function App() {
     window.setTimeout(() => {
         setKittyWithBallIsVisible(false)
     }, 2000)
-    //setNewKittyPosition(210)
+    window.setTimeout(() => {
+      const numberOfSwitches = 5;
+      for(let i = 0; i < numberOfSwitches; i++){
+        setKittyWithBallIsVisible(false)
+        window.setTimeout(() => {
+          setNewKittyPosition(positionConfigurations[getRandomIntInclusive(1, 5)])
+        }, i * 2000)
+      }
+    }, 2000)
   }
 
   return (
@@ -35,9 +56,9 @@ function App() {
       <div className="copyright">Icons made by <a href="https://www.flaticon.com/authors/smashicons" 
       title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       <div className="game" onClick={foo}>
-        <img src={happy} /*style={{left: newKittyPosition}}*/ className="kitty first" alt="kitty" data-nr={0}/>
-        <img src={happy} /*style={{left: newKittyPosition}}*/ className="kitty second" alt="kitty" data-nr={1}/>
-        <img src={happy} /*style={{left: newKittyPosition}}*/ className="kitty third" alt="kitty" data-nr={2}/>
+        <img src={happy} style={{left: newKittyPosition[0]}} className="kitty first" alt="kitty" data-nr={0}/>
+        <img src={happy} style={{left: newKittyPosition[1]}} className="kitty second" alt="kitty" data-nr={1}/>
+        <img src={happy} style={{left: newKittyPosition[2]}} className="kitty third" alt="kitty" data-nr={2}/>
         <img src={wool} id="wool" style={{left: ballLeft}} className={kittyWithBallIsVisible ? "visibleWool" : "wool"} alt="wool" />
         <button className="button" onClick={buttonClicked}>Start</button>
       </div>
